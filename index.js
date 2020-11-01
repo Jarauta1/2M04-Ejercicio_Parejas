a98cff07727c88c3d7d695ec824127180c11be86
Diego Jarauta <diegojarauta8@gmail.com>
2020-11-01 11:53:36 +0100

    Backend terminado, esperando el front

--- index.js
+++ index.js
@@ -33,4 +33,128 @@ app.get("/almacen/:seccion", function(req, res) {
 
 })
 
+app.post("/add", function(req, res) {
+    let seccion = req.body.seccion
+    seccion = seccion.toLowerCase()
+    let add = {
+        nombre = req.body.nombre,
+        descripccion = req.body.descripccion,
+        img = req.body.img,
+        precio = rez.body.precio,
+    }
+
+    let boolean = false
+
+    if (seccion == "armarios") {
+        almacen.armarios.push(seccion)
+        boolean = true
+    } else if (seccion == "mesas") {
+        almacen.mesas.push(seccion)
+        boolean = true
+    } else if (seccion == "sillas") {
+        almacen.sillas.push(seccion)
+        boolean = true
+    }
+    if (boolean = false) {
+        res.send({ error: true, mensaje: "Esa seccion no existe" })
+    }
+})
+
+app.put("/editar", function(req.res) {
+    let seccion = req.body.seccion
+    seccion = seccion.toLowerCase()
+    let nombre = req.body.nombre
+    let nombreCambiado = req.body.nombreCambiado
+    let imgCambiado = req.body.imgCambiado
+    let descripccionCambiado = req.body.descripccionCambiado
+    let precioCambiado = req.body.precioCambiado
+
+    let booleanSeccion = false
+    let booleanProducto = false
+
+    if (seccion == "armarios") {
+        for (let i = 0; i < almacen.armarios.length; i++) {
+            almacen.armarios[i].nombre = nombreCambiado
+            almacen.armarios[i].img = imgCambiado
+            almacen.armarios[i].descripccion = descripccionCambiado
+            almacen.armarios[i].precio = precioCambiado
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    } else if (seccion == "mesas") {
+        for (let i = 0; i < almacen.mesas.length; i++) {
+            almacen.mesas[i].nombre = nombreCambiado
+            almacen.mesas[i].img = imgCambiado
+            almacen.mesas[i].descripccion = descripccionCambiado
+            almacen.mesas[i].precio = precioCambiado
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    } else if (seccion == "sillas") {
+        for (let i = 0; i < almacen.sillas.length; i++) {
+            almacen.sillas[i].nombre = nombreCambiado
+            almacen.sillas[i].img = imgCambiado
+            almacen.sillas[i].descripccion = descripccionCambiado
+            almacen.sillas[i].precio = precioCambiado
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    }
+
+    if (booleanSeccion == false) {
+        res.send({ error: true, mensaje: "No existe esa seccion" })
+    }
+
+    if (booleanProducto == false) {
+        res.send({ error: true, mensaje: "No existe ese producto" })
+    }
+
+})
+
+app.delete("/borrar", function(req, res) {
+    let seccion = req.body.seccion
+    seccion = seccion.toLowerCase()
+    let nombre = req.body.nombre
+
+    let booleanSeccion = false
+    let booleanProducto = false
+
+    if (seccion == "armarios") {
+        for (let i = 0; i < almacen.armarios.length; i++) {
+            if (nombre == almacen.armarios[i].nombre)
+                almacen.armarios[i].splice(i, 1)
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    } else if (seccion == "mesas") {
+        for (let i = 0; i < almacen.mesas.length; i++) {
+            if (nombre == almacen.mesas[i].nombre)
+                almacen.mesas[i].splice(i, 1)
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    } else if (seccion == "sillas") {
+        for (let i = 0; i < almacen.sillas.length; i++) {
+            if (nombre == almacen.sillas[i].nombre)
+                almacen.sillas[i].splice(i, 1)
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    }
+
+    if (booleanSeccion == false) {
+        res.send({ error: true, mensaje: "No existe esa seccion" })
+    }
+
+    if (booleanProducto == false) {
+        res.send({ error: true, mensaje: "No existe ese producto" })
+    }
+})
+
 app.listen(3000);
